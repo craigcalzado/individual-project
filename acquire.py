b41706e8b3d223903879eb516bbcf79b7bd4d80a
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-04-26 14:11:51 -0500

    Updated functions

--- acquire.py
+++ acquire.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import requests
+import env
 
 def epi_category(category_id):
-    """ Returns the name of the category with the given id."""
-    url = 'https://api.eia.gov/category/?api_key=9eVTCpuQVgMQDZnriiK5gt3uNzwJMzzf4dBzvE2s&category_id=' + category_id
+    """ Returns the name of the category with the given id. Initial id key is  711238"""
+    key = env.api_key
+    url = 'https://api.eia.gov/category/?api_key=' + key + '&category_id=' + category_id
     result = requests.get(url)
     result.status_code
     result.json()
     return result.json()
+
+def data_manipulation(data):
+    """ Returns a dataframe of the series with the given id."""
+    # convert to dataframe to view data i would like to view
+    df_series = pd.DataFrame(data['category']['childseries'])
+    df_series
+    # select only the f rows that have 'A'
+    df_series[df_series['f'] == 'A']
+    series_id = []
+    series_name = []
+    for i in range(len(df_series)):
+        if df_series['f'][i] == 'M':
+            series_id.append(df_series['series_id'][i])
+            series_name.append(df_series['name'][i])
+    print(series_id)
+    print(series_name)
+    return series_id, series_name
 
     # create a function that turns the data into a dataframe
 def build_df(series_id):
@@ -17,11 +36,11 @@ def build_df(series_id):
     # seperate the 'data'
     df_dict = df['data'].to_dict()
     # convert to dataframe
-    df_data = pd.DataFrame(df_dict)
+    # df_data = pd.DataFrame(df_dict)
     # convert to list
-    df_data[0].tolist()
+    # df_dict[0].tolist()
     # convert list to dataframe
-    df = pd.DataFrame(df_data[0].tolist())
+    df = pd.DataFrame(df_dict[0])
     df.columns = ['date', 'value']
     return df
 
@@ -44,3 +63,4 @@ def epi_series(category_id):
     result.status_code
     result.json()
     return result.json()
+
